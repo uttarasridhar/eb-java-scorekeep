0ef3087bc72e953b13cb789a002ced48c676eba1
Uttara Sridhar <uttara@amazon.com>
2019-11-21 00:13:28 -0800

    Add print statement for troubleshooting

--- scorekeep-frontend/docker-entrypoint.sh
+++ scorekeep-frontend/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-echo "################################## Run nginx"
+echo "################################## Run nginx with alb domain name=$ECS_PROJECT_LB_DNS. Also, ${ECS_PROJECT_LB_DNS}"
 envsubst '${ECS_PROJECT_LB_DNS}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 exec "$@"
